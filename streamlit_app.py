96e71cd34534c4a70dc22e365a5561a0587447b8
ha4522843-hash <ha4522843@gmail.com>
2026-01-20 19:20:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -122,7 +122,7 @@ else:
     """, unsafe_allow_html=True)
 
     # عرض العلامة المائية
-else:
+
     st.markdown('<img src="https://i.ibb.co/WWq0wnpg/Layer-8.png" class="watermark">', unsafe_allow_html=True)
     # السايد بار
     with st.sidebar:
@@ -142,6 +142,7 @@ else:
     st.title("Clinic Control Center")
     st.success("Welcome Dr. Bahaa! The system is ready.")
     st.info("الصور الآن تعمل بأعلى نقاء (HD) وبدون بكسلة.")
+
 
 
 
